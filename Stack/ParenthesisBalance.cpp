036693e84f2099c7ca26693aa9dbe85d7b9f8ef7
Shubho Shaha <shubhodotcse@users.noreply.github.com>
2016-10-13 21:45:49 +0600

    Create ParenthesisBalance.cpp

---
+++ Stack/ParenthesisBalance.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isOpening(char ch){
+    if(ch=='(' or ch=='{' or ch=='[')
+        return true;
+    else return false;
+}
+bool isCloseing(char ch){
+    if(ch==')' or ch=='}' or ch==']')
+        return true;
+    else return false;
+}
+bool isMatched(char op,char close){
+    if( (op=='(' and close==')') or (op=='{' and close=='}') or (op=='[' and close==']') )
+        return true;
+    else
+        return false;
+}
+bool isBalanced(string inp){
+    stack<int>st;
+    for(int i = 0;i<inp.size();i++){
+        if(isOpening(inp[i])){
+            st.push(inp[i]);
+            cout<<inp[i]<<" "<<i<<endl;
+        }
+        else if(isCloseing(inp[i])){
+            char ch = st.top();
+            //cout<<ch;
+            st.pop();
+            if(!isMatched(ch,inp[i])){
+                cout<<ch<<" "<<inp[i]<<" "<<i<<endl;
+                return false;
+            }
+        }
+    }
+    if(!st.empty())
+        return false;
+    else return true;
+}
+int main(){
+    string inp ="[()]{}{[()()]()}";
+    isBalanced(inp)?cout<<"Yes":cout<<"No";
+}
