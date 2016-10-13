c6f9d64ce81da51ca04942f87f142d5e6c3dec23
Shubho Shaha <shubhodotcse@users.noreply.github.com>
2016-10-13 21:44:59 +0600

    Create MinFromStack.cpp

---
+++ Stack/MinFromStack.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+stack<int> st,minimum;
+void Push(int n){
+    st.push(n);
+    if(minimum.empty()){
+        minimum.push(n);
+    }
+    else if(minimum.top()>=n){
+        minimum.push(n);
+    }
+}
+void Pop(){
+    if(st.empty())
+        return ;
+    int t = st.top();
+    //cout<<"TOP"<<t<<endl;
+    st.pop();
+    if(minimum.top()==t)
+        minimum.pop();
+}
+int GetMinimum(){
+    if(!minimum.empty())
+        return minimum.top();
+    else
+        return -1;
+}
+int main(){
+    int c;
+    cout<<"1. Push"<<endl
+        <<"2. Pop"<<endl
+        <<"3. Get Minimum"<<endl;
+    while(cin>>c){
+        switch(c){
+        case 1:
+            int item;
+            cout<<"Push Data"<<endl;
+            cin>>item;
+            Push(item);
+            break;
+        case 2:
+            Pop();
+            break;
+        case 3:
+            cout<<"Minimum Value" <<" "<<GetMinimum()<<endl;
+            break;
+        default:
+            return 0;
+        }
+    }
+}
