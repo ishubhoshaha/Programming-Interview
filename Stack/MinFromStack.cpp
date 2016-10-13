0cf0e8bddf16a747c5cb2f8c84ca0a968390ebef
Shubho Shaha <shubhodotcse@users.noreply.github.com>
2016-10-13 22:01:12 +0600

    Update MinFromStack.cpp

--- Stack/MinFromStack.cpp
+++ Stack/MinFromStack.cpp
@@ -1,4 +1,6 @@
-/* http://stackoverflow.com/questions/685060/design-a-stack-such-that-getminimum-should-be-o1 */
+/* 
+* Reference: http://stackoverflow.com/questions/685060/design-a-stack-such-that-getminimum-should-be-o1 
+*/
 #include<bits/stdc++.h>
 using namespace std;
 stack<int> st,minimum;
