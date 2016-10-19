f98e30b088ac5a9a20ad68659abdcbd297bb7916
Shubho Shaha <shubhodotcse@users.noreply.github.com>
2016-10-19 13:58:15 +0600

    KeyIdea
    1. Sort the Array
    2. Iterate over the sorted array while l < r.
           (a) If (A[l] + A[r] == sum)  then return 1
           (b) Else if( A[l] + A[r] <  sum )  then l++
           (c) Else r--  
    
    Reference: http://www.geeksforgeeks.org/write-a-c-program-that-given-a-set-a-of-n-numbers-and-another-number-x-determines-whether-or-not-there-exist-two-elements-in-s-whose-sum-is-exactly-x/

---
+++ Arrays/PairThatMake_N.cpp
@@ -0,0 +1,30 @@
+/*
+* Time Complexity : O(nlogn)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int arr[500];
+int main(){
+    int n,make;
+    cin>>n>>make;
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    int left = 0,right = n-1;
+    while(left<right){
+        if(arr[left]+arr[right]==make)
+        {
+            cout<<arr[left]<<" "<<arr[right]<<endl;
+            --right;
+            ++left;
+
+        }
+        else if(arr[left]+arr[right]>make){
+            --right;
+        }
+        else if(arr[left]+arr[right]<make){
+            ++left;
+        }
+    }
+}
