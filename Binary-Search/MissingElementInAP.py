d02630b7a2f25c20993e536e9063282819522c24
Shubho Shaha <shubhodotcse@users.noreply.github.com>
2017-01-11 23:28:00 +0600

    Create MissingElementInAP.py

---
+++ Binary-Search/MissingElementInAP.py
@@ -0,0 +1,18 @@
+def BinarySearch(lst):
+    n = len(lst)
+    start = 0
+    ends = n-1
+    difference = (lst[n-1]-lst[0])/n
+    while(start<ends):
+        mid = (start+ends)/2
+        if abs(lst[mid]-lst[mid-1])!=difference:
+            return lst[mid-1]+difference
+        elif abs(lst[mid]-lst[mid+1])!=difference:
+            return lst[mid]+difference
+        elif (lst[start]+mid*difference) == lst[mid]:
+            start = mid+1
+        else:
+            ends = mid-1
+
+lst = [int(x) for x in raw_input().split()]
+print BinarySearch(lst)
