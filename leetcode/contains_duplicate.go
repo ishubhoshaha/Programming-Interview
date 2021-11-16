68954ed1a28e88ebe41f3577cbe39a7d466ac182
Shubho Shaha <7293317+ishubhoshaha@users.noreply.github.com>
2021-11-16 13:47:16 +0600

    Create contains_duplicate.go

---
+++ leetcode/contains_duplicate.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func containsDuplicate(nums []int) bool {
+	mp := make(map[int]bool)
+	for i := 0; i<len(nums); i++ {
+		if _, exist := mp[nums[i]]; exist {
+			return true
+		} else {
+			mp[nums[i]] = true
+		}
+	}
+	return false
+}
+
+func main()  {
+	var a []int
+	a = make([]int, 4)
+	a [0] = 1
+	a [1] = 2
+	a [2] = 3
+	a [3] = 1
+	r := containsDuplicate(a)
+	fmt.Println(r)
+
+}
